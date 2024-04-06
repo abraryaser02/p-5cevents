23e4b3efb3605b601b55f61538897195de7ba87c
Yunju Song <gpyunju2004@gmail.com>
2024-04-06 16:19:20 -0700

    VERY basic login page without backend implementation

--- services/client/my-app/src/App.js
+++ services/client/my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import CalendarPage from './CalendarPage';
 import EventPage from './EventPage';
+import LoginPage from './Login';
 import MapPage from './MapPage';
 import NotFoundPage from './NotFoundPage';
 
@@ -10,7 +11,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route exact path="/" element={<EventPage />} /> {/* Default route */}
+          <Route exact path="/" element={<LoginPage />} /> {/* Default route */}
           <Route path="/Events" element={<EventPage />} />
           <Route path="/Calendar" element={<CalendarPage />} />
           <Route path="/Map" element={<MapPage />} />
---
+++ services/client/my-app/src/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate from react-router-dom
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  // Initialize useNavigate hook
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Figure out backend stuff! 
+    // Use temporary email and password for now to do login for demo
+    if (email === 'example@example.com' && password === 'password') {
+      setIsLoggedIn(true);
+      alert('Login successful!');
+      // Go to events page when logged in for now, create separate homepage in the future(?)
+      navigate('/events');
+    } else {
+      alert('Invalid email or password');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
